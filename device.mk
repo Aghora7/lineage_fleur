0c8aecdadc68872088d7a6e4a51071b8d4772851
Roshan Pius <rpius@google.com>
2022-04-29 19:42:03 +0100

    fleur: rro_overlays: Add WifiOverlay Bug: 148617260 Test: Device boots up and connects to wifi networks.
    Change-Id: Iab6ec7ff4a24e7e92f97231f139e31cb57e7501f

--- device.mk
+++ device.mk
@@ -211,6 +211,9 @@ PRODUCT_PACKAGES += \
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
 
+PRODUCT_PACKAGES += \
+    WifiOverlay
+
 # Public Libraries
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt \
@@ -275,7 +278,7 @@ PRODUCT_SOONG_NAMESPACES += \
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.3-service-mediatekv2
-    
+
 # WiFi
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
