e89d5f4c03ae558cf60a8d96c4c0f63da9c64ae5
Matsvei Niaverau <matvej2002@gmail.com>
2022-12-04 13:07:08 +0100

    fleur: Build sensors multihal from source
    Change-Id: Iebccb00d08d15bfb466cff9de4aec435b13109e5

--- device.mk
+++ device.mk
@@ -405,6 +405,7 @@ PRODUCT_COPY_FILES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
+    android.hardware.sensors@2.0-service.multihal \
     android.hardware.sensors@1.0.vendor \
     android.hardware.sensors@2.0.vendor \
     android.frameworks.sensorservice@1.0
