e5370c66e69241f92f412096cc84a78a4f5375af
bengris32 <bengris32@protonmail.ch>
2023-07-21 13:10:34 +0100

    fleur: Ship MediaTek cgroup/task_profiles configuration
    * MediaTek ship their own modified task_profiles and
      use the V30 cgroups configuration.
    
    Change-Id: I15245ea87b00925597db65f1564e808a094c937a
    Signed-off-by: bengris32 <bengris32@protonmail.ch>

--- device.mk
+++ device.mk
@@ -150,6 +150,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libcamera_metadata_shim
 
+# Cgroup
+PRODUCT_COPY_FILES += \
+    system/core/libprocessgroup/profiles/cgroups_30.json:$(TARGET_COPY_OUT_VENDOR)/etc/cgroups.json \
+    $(LOCAL_PATH)/configs/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
+    
 # Characteristics    
 PRODUCT_CHARACTERISTICS := nosdcard
 
