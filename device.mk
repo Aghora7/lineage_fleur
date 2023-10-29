ea186b301d0324c3ac7dfd48c13b18292373c7f4
Bruno Martins <bgcngm@gmail.com>
2023-10-29 19:29:34 +0000

    fleur: Migrate to AIDL ClearKey DRM HAL
    Change-Id: Iad5cc1176978137c647069197b33f49fb4d98ab2

--- device.mk
+++ device.mk
@@ -155,7 +155,7 @@ PRODUCT_CHARACTERISTICS := nosdcard
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.4-service.clearkey
+    android.hardware.drm-service.clearkey
 
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0.vendor \
