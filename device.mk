be4db6ec09309f23e67d01ebaa0a0bc859767f4f
bengris32 <bengris32@protonmail.ch>
2024-02-10 16:00:42 +0530

    fleur: Initial reverse engineered Goodix fingerprint 2.1 interfaces
    Change-Id: Idd04d3313a6dbacd649767ff686426de152d576d

--- device.mk
+++ device.mk
@@ -192,6 +192,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.3-service.xiaomi
 
+PRODUCT_PACKAGES += \
+    libvendor.goodix.hardware.biometrics.fingerprint@2.1.vendor
+    
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.fingerprint.xml
     
