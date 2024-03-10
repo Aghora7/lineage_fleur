a6ceba1ef34446800adf7e31a50e2c355eeee58e
aghora7 <ukikieran@gmail.com>
2024-03-10 15:40:27 +0530

    fleur: import rootdir files
    Signed-off-by: aghora7 <ukikieran@gmail.com>

--- device.mk
+++ device.mk
@@ -7,6 +7,44 @@
 # Enable updating of APEXes
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
+# Rootdir
+PRODUCT_PACKAGES += \
+    init.insmod.sh \
+
+PRODUCT_PACKAGES += \
+    fstab.mt6781 \
+    fstab.enableswap \
+    factory_init.connectivity.common.rc \
+    factory_init.connectivity.rc \
+    factory_init.project.rc \
+    factory_init.rc \
+    init.aee.rc \
+    init.ago.rc \
+    init.cgroup.rc \
+    init.connectivity.common.rc \
+    init.connectivity.rc \
+    init.modem.rc \
+    init.mt6781.rc \
+    init.mt6781.usb.rc \
+    init.project.rc \
+    init.sensor_1_0.rc \
+    init.stnfc.rc \
+    init_connectivity.rc \
+    meta_init.connectivity.common.rc \
+    meta_init.connectivity.rc \
+    meta_init.modem.rc \
+    meta_init.project.rc \
+    meta_init.rc \
+    multi_init.rc \
+    init.recovery.hardware.rc \
+    init.recovery.mt6781.rc \
+    miui.factoryreset.rc \
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/rootdir/etc/fstab.mt6781:$(TARGET_VENDOR_RAMDISK_OUT)/first_stage_ramdisk/fstab.mt6781 \
+    $(LOCAL_PATH)/rootdir/etc/init.mt6781.rc:$(TARGET_COPY_OUT_SYSTEM)/etc/init/init.mt6781.rc \
+    $(LOCAL_PATH)/rootdir/etc/fstab.mt6781:$(TARGET_COPY_OUT_RECOVERY)/root/first_stage_ramdisk/fstab.mt6781 \
+
 # Shipping API level
 PRODUCT_SHIPPING_API_LEVEL := 30
 
